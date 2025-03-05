cab7cb0163add4f4ffc3b9b8ba1c77641942204b
franwrld <franwrld22@gmail.com>
2025-03-05 02:35:06 -0600

    Se han borrado archivos user porque no hacen falta debido a la nueva estructura

--- app/controllers/nuevacuentasusercontroller.php
+++
@@ -1,7 +0,0 @@
-<?php
-class NuevaCuentasuserController extends Controller {
-
-    public function __construct($parametro) {
-        parent::__construct("nuevacuentasuser",$parametro,true);
-    }
-}
--- app/models/acercadeuser.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once "app/models/db.class.php";
-class Acercadeuser extends BaseDeDatos {
-
-    public function __construct() {
-        $this->conectar();
-    }
-
-} 
